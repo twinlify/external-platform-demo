3956c714fe023021af7d3788951f272b89da930b
chrisdugne <chris.dugne@uralys.com>
2021-01-21 21:20:30 +0100

    applied data selection

--- src/app/demos/demo1/containers/menu.js
+++ src/app/demos/demo1/containers/menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import $Button from '../../../components/button';
 import {device, mediaQueryTransition} from '../../../style/breakpoints';
@@ -18,6 +18,7 @@ const $Menu = styled.div`
   background-color: #222;
   height: calc(100vh - 80px);
   padding: 10px;
+  font-size: 14px;
   box-sizing: border-box;
   width: 80px;
 
@@ -25,32 +26,40 @@ const $Menu = styled.div`
     width: 300px;
   }
 
+  ${mediaQueryTransition}
+`;
+
+const $List = styled.div`
+  height: calc(100vh - 180px);
   overflow-x: hidden;
   overflow-y: scroll;
-
-  ${mediaQueryTransition}
 `;
 
 // -----------------------------------------------------------------------------
 
-const Entry = ({name, value, date}) => (
-  <$$Button
-    selected={false}
-    // onClick={handleClick(num)}
-  >
-    {name}
-  </$$Button>
-);
+const Menu = ({timeSeries}) => {
+  const [selectedNum, selectNum] = useState(0);
+  const handleClick = num => () => selectNum(num);
 
-// -----------------------------------------------------------------------------
-
-const Menu = ({timeSeries}) => (
-  <$Menu>
-    {timeSeries.map(([date, value], index) => (
-      <Entry key={index} name={`data-${index}`} value={value} date={date} />
-    ))}
-  </$Menu>
-);
+  return (
+    <$Menu>
+      <p>Selection: {selectedNum}</p>
+      <$List>
+        {timeSeries.map(([date, value], index) => (
+          <$$Button
+            onClick={handleClick(index)}
+            key={index}
+            selected={index === selectedNum}
+            value={value}
+            date={date}
+          >
+            {`data-${index}`}
+          </$$Button>
+        ))}
+      </$List>
+    </$Menu>
+  );
+};
 
 // -----------------------------------------------------------------------------
 
