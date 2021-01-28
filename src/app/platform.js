01010cf3dab3181fbfc764088dece1fdfd8dc419
chrisdugne <chris.dugne@uralys.com>
2021-01-28 21:01:49 +0100

    updated nexus to 0.4.4

--- src/app/platform.js
+++ src/app/platform.js
@@ -36,7 +36,7 @@ const $App = styled.div`
 
 // -----------------------------------------------------------------------------
 
-const NEXUS_VERSION = '0.4.3';
+const NEXUS_VERSION = '0.4.4';
 const localCss = `http://localhost/nexus-dist/min/nexus-${NEXUS_VERSION}.css`;
 const localJs = `http://localhost/nexus-dist/min/nexus-${NEXUS_VERSION}.debug.js`;
 
