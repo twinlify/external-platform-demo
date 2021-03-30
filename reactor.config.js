f591d84eb21669e82d7f615124ec6596a4eae4c1
chrisdugne <chris.dugne@uralys.com>
2021-03-30 18:23:16 +0200

    removed prebuild -> nexus now provides esm

--- reactor.config.js
+++ reactor.config.js
@@ -1,14 +1,6 @@
-const shell = require('shelljs');
-
 module.exports = {
-  prebuild: () => {
-    console.warn(
-      'warning: removing three `main` file to avoid duplicate in bundle'
-    );
-    shell.rm('-f', 'node_modules/three/build/three.js');
-  },
   esbuild: {
-    // metafile: true,
+    metafile: true,
     loader: {
       '.js': 'jsx',
       '.md': 'text'
