6b4187874a87fa7e94917c2fee29be3d7e8e4865
chrisdugne <chris.dugne@uralys.com>
2021-06-07 14:47:59 +0200

    added sitemap generation

--- reactor.config.js
+++ reactor.config.js
@@ -9,5 +9,14 @@ module.exports = {
   },
   start: {
     hosts: ['platform.localhost']
+  },
+  sitemap: {
+    publicPath: './public',
+    hostname: 'https://platform.twinlify.com',
+    links: [
+      {url: '/', lastmod: new Date()},
+      {url: '/demo1/', lastmod: new Date()},
+      {url: '/demo2/', lastmod: new Date()}
+    ]
   }
 };
